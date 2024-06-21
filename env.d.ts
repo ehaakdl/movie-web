1ad07dfb1738de476f1a8d9fd3168a65a4c0cbf4
mosehun <ehaakdl@naver.com>
2024-06-21 21:40:58 +0900

    feature: 공통 타입 선언

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,14 @@
-// 환경변수 유형을 정의하는 파일이다. 이거 있어야 import.meta.env.{환경변수} 했을떄 에러 안난다.
-interface ImportMeta {
-    env: {
-        VITE_BASE_URL?: string,
-    }
-}
+/**
+ * TypeScript 프로젝트에서 전역적으로 사용할 수 있는 타입 선언을 정의하는 파일입니다.
+ * 주로 환경 변수의 타입을 정의하거나,
+ * 프로젝트 내에서 공통적으로 사용될 수 있는 타입 선언을 포함합니다.
+ * 이 파일은 TypeScript 컴파일러가 프로젝트의 모든 파일에서 참조할 수 있는 타입 정보를 제공하여,
+ * 타입 안전성과 코드 완성 기능을 향상시킵니다.
+ */
+
+// 아래 타입들을 전역적으로 참조할수 있게 설정한 코드입니다.
+// https://www.typescriptlang.org/docs/handbook/triple-slash-directives.html
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-vue-layouts/client" />
+/// <reference types="unplugin-vue-router/client" />
+
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,10 +6,17 @@
 
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
+/**
+ * virtual:generated-layouts는 Vite 프로젝트에서 자동으로 레이아웃을 생성하고
+ * 관리할 수 있게 도와주는 가상 모듈입니다.
+ * 이는 vite-plugin-vue-layouts 플러그인을 통해 제공됩니다.
+ * 이 플러그인은 파일 시스템을 기반으로 레이아웃을 자동으로 인식하고,
+ * 필요한 설정을 자동으로 적용해 줍니다.
+ */
 import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   extendRoutes: setupLayouts,
 })
 
